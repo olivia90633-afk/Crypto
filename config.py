52189035a8d264f8cd54ea9666fe52e1c370614d
olivia90633-afk <olivia90633@gmail.com>
2026-04-02 00:57:29 +0100

    Replace placeholder tokens with actual values
    Updated BOT_TOKEN and CHAT_ID with actual values.

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
-CHAT_ID = "YOUR_TELEGRAM_CHAT_ID"  # can be your user ID or a channel ID
+BOT_TOKEN = "8242593757:AAHoIOP1pcXwPPGto5KAHg3A_gVctmrGhAU"
+CHAT_ID = "7926402001"  # can be your user ID or a channel ID
